c768a64ea6c3d3c010cbedd2a9eb55da71ae49a9
Andresito86 <bergicsandras@gmail.com>
2022-11-22 16:07:18 +0100

    [IMP] AL-2

---
+++ sort/bubble_sort/andris.py
@@ -0,0 +1,22 @@
+
+def swapelements(lista, index):
+    nextelement = lista[index+1]
+    if lista[index] > lista[index+1]:
+        lista[index], lista[index+1] = nextelement, lista[index]
+
+def firstiteration(lista, length):
+    for i in range (0, length-1):
+        swapelements(lista, i)
+
+
+def bubblesort(lista):
+    i=len(lista)
+    while i>0:
+        firstiteration(lista,i)
+        i-=1
+
+
+
+testlista = [10,4,2,6,3,5]
+bubblesort(testlista)
+print(testlista)
--- tests/test_bubble_sort.py
+++ tests/test_bubble_sort.py
@@ -5,30 +5,34 @@ from algorithms.sort.bubble_sort import andris
 from algorithms.sort.bubble_sort import grzs
 
 
-data1 = [5, 1, 3, 2, 4]
 
 
-# class TestBubbleSortAndris(unittest.TestCase):
-#     def test_first_step(self):
-#         """
-#         Test that it swaps the first two element first
-#         """
-#         result = andris.swap_first_two(data1)
-#         self.assertEqual(result, [1, 5, 3, 2, 4])
 
-#     def test_first_round(self):
-#         """
-#         Test that it can find the greatest item
-#         """
-#         result = andris.greatest_to_end(data1)
-#         self.assertEqual(result, [1, 3, 2, 4, 5])
+class TestBubbleSortAndris(unittest.TestCase):
+    def test_first_step(self):
 
-#     def test_sort(self):
-#         """
-#         Test that it can sort a list of integers
-#         """
-#         result = andris.sort(data1)
-#         self.assertEqual(result, [1, 2, 3, 4, 5])
+        """
+        Test that it swaps the first two element first
+        """
+        data1 = [5, 1, 3, 2, 4]
+        andris.swapelements(data1,0)
+        self.assertEqual([1, 5, 3, 2, 4], data1)
+
+    def test_first_round(self):
+        """
+        Test that it can find the greatest item
+        """
+        data1 = [5, 1, 3, 2, 4]
+        andris.firstiteration(data1, len(data1))
+        self.assertEqual([1, 3, 2, 4, 5],data1)
+
+    def test_sort(self):
+        """
+        Test that it can sort a list of integers
+        """
+        data1 = [5, 1, 3, 2, 4]
+        andris.bubblesort(data1)
+        self.assertEqual([1, 2, 3, 4, 5],data1)
 
 
 class TestBubbleSortGrzs(unittest.TestCase):
